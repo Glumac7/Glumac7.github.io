11b12a791ca511fe55cc1b2118dce5d66f2a1ffb
Nemanja Glumicic <necaglumicic@gmail.com>
2018-07-08 18:26:58 +0200

    First push

---
+++ js/javascript.js
@@ -0,0 +1,72 @@
+function main_page()
+{
+	document.querySelector('nav').style.display = "flex";
+	document.querySelector('main').style.display = "block";
+	document.querySelector('#loader').style.display = "none";
+}
+
+/*MOBILE BURGER ICON!*/
+function burger_icon()
+{
+	var buton = document.getElementById("nav-but");
+	var burger = document.querySelector('.burger-container');
+
+	if (burger.classList) 
+	{ 
+	    burger.classList.toggle("open");
+	} 
+	else 
+	{
+	    // For IE9
+	    var classes = burger.className.split(" ");
+	    var i = classes.indexOf("open");
+
+	    if (i >= 0) 
+	        classes.splice(i, 1);
+	    else 
+	        classes.push("open");
+	        burger.className = classes.join(" "); 
+	}
+}
+
+/*HEADER SCROLL EFECT!*/
+window.addEventListener("scroll", function()
+{
+	if(document.body.offsetWidth <= 991)
+	{}
+	else
+	{
+		var scrollTop = window.pageYOffset || document.body.scrollTop;
+		var logo = document.querySelector('.navbar-brand');
+		var nav = document.querySelector('#nav');
+		var links = document.getElementById('nav-ul').children;
+
+		if(scrollTop >= 1)
+		{
+			nav.style.backgroundColor = "rgba(255, 255, 255, .95)";
+			logo.style.fontSize = "30px";
+			logo.style.color = "#0090e0";
+		    
+		    for (let i = 0; i < links.length; i++) 
+		    {
+		    	var a_tags;
+		        a_tags = links[i].children;
+		        a_tags[0].style.color = "#606060";
+		    }
+		}
+		else
+		{
+			nav.style.backgroundColor = "rgba(0,0,0,0)";
+			logo.style.fontSize = "40px";
+			logo.style.color = "#eee";
+			
+		    for (let i = 0; i < links.length; i++) 
+		    {
+		    	var a_tags;
+		        a_tags = links[i].children;
+		        a_tags[0].style.color = "#eee";
+		    }
+		}
+	}
+
+}, false)
---
+++ js/jq.js
@@ -0,0 +1,89 @@
+$(document).ready(() =>
+{	
+	/*SCROLL DOWN BUTTON!*/
+	$("a").on('click', function(event) 
+	{
+	    if (this.hash !== "") 
+	    {
+	      event.preventDefault();
+	      var hash = this.hash;
+
+	      $('html, body').animate(
+	      {
+	        scrollTop: $(hash).offset().top
+	      }, 635, function()
+	      {
+	        window.location.hash = hash;
+	      });
+	    } 
+	});
+
+	
+	$(window).scroll(function() 
+	{
+		/*ABOUT ME ANIMATIONS!*/
+		/*--------------------*/
+		/*VARIALBLE DECLARATION!*/
+		$body_width = $('body').width();
+		$body_height = $('body').height();
+
+		/*IF NOT ON MOBILE!*/
+	    if($(this).scrollTop() >= $body_height - 150 && $body_width > 800)
+	    {
+	    	/*ADD A CLASS TO A SPECIFIC SKILL BAR!*/
+	    	for(var i = 1 ; i < 9 ; i++)
+	    	{
+	    		$(".progress:nth-child("+ i +")").children("div.progress-bar").addClass('prog' + i + '');
+	    	}
+
+			$('.progress').css({'display':'flex'});/*DISPLAY THE PROGRESS BARS!*/
+	    	$('#skill-bar').addClass('fadeInRight');/*ADD AN ANIMATION TO THE SKILL BAR!*/
+	    	$('#about-img').addClass('fadeInLeft');/*ADD AN ANIMATION TO THE IMAGE!*/
+	    	$('#about-img, .about_me').css({'visibility': 'visible'});/*DISPLAY THE IMAGE AND THE TEXT BENEATH IT!*/
+	    	$('.about_me').addClass('fadeInUp');/*ADD AN ANIMATION TO THE TEXT BENEATH THE IMAGE!*/
+
+	    	
+	    }
+	    /*********************************************/
+	    /*VARIALBLE DECLARATION!*/
+		$about_height = $('#about').height() + $body_height;
+
+	    /*PROJECTS ANIMATIONS!*/
+	    if($(this).scrollTop() >= $about_height - 150 && $body_width > 800)
+	    {
+	    	$("#project-container").css({"opacity": "1"});
+	    	$("div#project-container").addClass('fadeInUp');
+	    }
+	    /*********************************************/
+	    /*ADD A CLASS TO A SPECIFIC CONTACT BUTTON!*/
+	    $contact_height = $('#projects').height() + $about_height;
+
+		if ($(this).scrollTop() >= $contact_height - 150) 
+		{
+			$("#map").addClass('fadeLeft');
+			$("#map2").addClass('fadeRight');
+		}
+		if ($(this).scrollTop() >= $contact_height + 100) 
+		{
+		    for(var i = 1 ; i < 9 ; i++)
+	    	{
+	    		$(".icons:nth-child("+ i +")").addClass('fadeUp').css({"animation-delay": i/10+"s"});
+	    	}
+		}
+   	});
+
+	/*PROJECTS HOVER ANIMATION!*/
+	if($('body').width() < 991)
+	{
+		$("figure").addClass("hover");
+	}
+  	else	
+  	{
+  		$(".hover").mouseleave(
+			function () 
+			{
+	 			$(this).removeClass("hover");
+			}
+		);
+  	}
+});
